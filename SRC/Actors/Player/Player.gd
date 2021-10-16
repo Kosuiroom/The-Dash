dedce09490c5c7db3f09aad9a14850bf619cef41
Redines <pontus.g91@gmail.com>
2021-10-16 15:17:37 +0200

    added simple dash mechanic

--- SRC/Actors/Player/Player.gd
+++ SRC/Actors/Player/Player.gd
@@ -7,6 +7,11 @@ export var jump : float = 3000.0
 export var gravity : float = 2500.0
 const UP : Vector2 = Vector2.UP
 var velocity = Vector2()
+var dashing = false
+export var dashSpeed = 1000
+var lerprate = 0.1
+var direction = 0
+var notMoving = true
 
 func _physics_process(delta):
 	apply_gravity(delta)
@@ -18,21 +23,37 @@ func apply_gravity(delta : float) -> void:
 	velocity.y = gravity * delta;
 
 func get_input(delta):
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("mvLeft"):
+		direction = -1
+		notMoving = false
 		speed_calc(delta)
-		velocity.x = -Speed
-		
-	elif Input.is_action_pressed("ui_right"):
+		velocity.x = lerp(velocity.x,-Speed,lerprate)
+	elif Input.is_action_pressed("mvRight"):
+		direction = 1
+		notMoving = false
 		speed_calc(delta)
-		velocity.x = Speed
+		velocity.x = lerp(velocity.x,Speed,lerprate)
 	else:
+		notMoving = true
 		Speed = 0
 		velocity.x = 0
+		
 	if is_on_floor():
 		if Input.is_action_just_pressed("player_jump"):
 			velocity.y -= jump
 			
+	if Input.is_action_just_pressed("Dash") && dashing == false && notMoving == false:
+		dash()
+
 func speed_calc(delta): 
 	Speed += acceleration * delta
 	if Speed > maxSpeed:
 		Speed = maxSpeed
+		
+func dash() -> void:
+	print("dash")
+	dashing = true
+	velocity.x = dashSpeed * direction
+	yield(get_tree().create_timer(0.3), "timeout")
+	dashing = false
+	pass
