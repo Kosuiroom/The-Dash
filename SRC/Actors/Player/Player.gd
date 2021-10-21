20b79d9b4fec6dc4eac757658353558b29094c06
DavidBystroem <a18davby@student.his.se>
2021-10-21 21:15:57 +0200

    Davids fingerprints
    New scene for the test level. Player animation set up (and a little in code) - Run, idle (working).  Parallax background. New test tilemaps (probably gonna take less pixel-sized later). Small camera adjustments. New files and assets + organized. Basically just learning things in godot for myself (wrong prioritizing probably).

--- SRC/Actors/Player/Player.gd
+++ SRC/Actors/Player/Player.gd
@@ -13,6 +13,7 @@ var dashing = false
 var lerprate = 0.1
 var direction = 0
 var notMoving = true
+onready var animation = $AnimationPlayer
 
 func _physics_process(delta):
 
@@ -31,24 +32,31 @@ func apply_gravity(delta : float) -> void:
 
 func get_input(delta):
 	if Input.is_action_pressed("mvLeft"):
+		animation.play("Running")
+		$Sprite.flip_h = true
 		direction = -1
 		notMoving = false
 		speed_calc(delta)
 		velocity.x = lerp(velocity.x,-Speed,lerprate)
 	elif Input.is_action_pressed("mvRight"):
+		animation.play("Running")
+		$Sprite.flip_h = false
 		direction = 1
 		notMoving = false
 		speed_calc(delta)
 		velocity.x = lerp(velocity.x,Speed,lerprate)
 	else:
 		notMoving = true
+		animation.play("Idle")
 		Speed = 0
 		velocity.x = lerp(velocity.x,Speed,lerprate)
 		
 	if is_on_floor():
 		if Input.is_action_just_pressed("player_jump"):
+			animation.play("Jump")
 			velocity.y -= jump
 			$Player_Jump.play()
+		
 			
 	if Input.is_action_just_pressed("Dash") && dashing == false && notMoving == false:
 		dash()
