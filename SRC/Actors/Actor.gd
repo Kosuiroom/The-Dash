c4a19da9570133168003a2c792a011d9992e62a6
Pontus <pontus.g91@gmail.com>
2021-11-28 17:39:21 +0100

    added slime health and player health, created moving platform

--- SRC/Actors/Actor.gd
+++ SRC/Actors/Actor.gd
@@ -3,4 +3,12 @@ class_name Actor
 
 var velocity: = Vector2.ZERO
 var speed: = Vector2(0,0)
+var health = 0
 export var gravity = 500
+
+
+func tookDmg(var ActorHealth, var damageTaken):
+	var newHealth = ActorHealth - damageTaken
+	print("actor took: ", damageTaken)
+	print("actors health is: ", newHealth)
+	return newHealth
