d469d828af35635b90990614af525cee6059004a
DavidBystroem <a18davby@student.his.se>
2022-02-05 23:55:39 +0100

    Breakable Platform implemented

---
+++ SRC/LevelObjects/BreakablePlatforms/BreakablePlatform.gd
@@ -0,0 +1,14 @@
+extends Area2D
+
+func _physics_process(delta):
+	var bodies = get_overlapping_bodies()
+	for body in bodies:
+		if body.name == "Player":
+			$AnimationPlayer.play("Destroyed")
+			yield($AnimationPlayer, "animation_finished")
+			queue_free()
+		else:
+			$AnimationPlayer.play("Idle")
+
+
+
